1b96e06ed04310819cdd6ba6d0f557ee583eecf5
OLIX <151432001+olix09@users.noreply.github.com>
2025-06-29 18:20:04 +0300

    Add files via upload

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,5 @@
--- task_6.sql - Insert multiple customer records
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    ('Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
